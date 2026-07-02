847b044025818d08e4dc35a051c4daa18b935daa
afcolmenaresb <afcolmenaresb@unal.edu.co>
2026-07-02 12:56:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const pageFlip = new StPageFlip(document.getElementById('book'), {
-    width: 350,
-    height: 500,
-    size: "fixed",
-    showCover: true,
-    mobileScrollSupport: true
-});
+document.addEventListener("DOMContentLoaded", function() {
+    const pageFlip = new StPageFlip(document.getElementById('book'), {
+        width: 350,
+        height: 500,
+        size: "fixed",
+        showCover: true
+    });
 
-pageFlip.loadFromHTML(document.querySelectorAll(".page"));
+    pageFlip.loadFromHTML(document.querySelectorAll(".page"));
+});
