0e6407dbd921e3af78c58e6a4aacec36e28be673
afcolmenaresb <afcolmenaresb@unal.edu.co>
2026-07-02 13:28:16 -0300

    Refactor pageFlip initialization and display logic
    Refactor book element initialization and visibility.

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const pageFlip = new StPageFlip(document.getElementById('book'), {
+    const bookElement = document.getElementById('book');
+    
+    // Inicializar el libro
+    const pageFlip = new StPageFlip(bookElement, {
         width: 350,
         height: 500,
         size: "fixed",
-        showCover: true
+        showCover: true,
+        mobileScrollSupport: true
     });
 
+    // Cargar las páginas
     pageFlip.loadFromHTML(document.querySelectorAll(".page"));
+
+    // Una vez cargado, lo hacemos visible
+    bookElement.style.display = 'block';
 });
